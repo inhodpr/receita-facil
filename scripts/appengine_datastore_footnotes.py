890a71dd88fd964cf36ef4779c74f979508f961f
Davi Pires <inhodpr@gmail.com>
2023-03-26 21:36:58 +0000

    Making datastore footnotes more robust.

--- scripts/appengine_datastore_footnotes.py
+++ scripts/appengine_datastore_footnotes.py
@@ -6,10 +6,14 @@ class AppEngineDataStoreFootnotes(storage.FootnotesStorage):
     def __init__(self):
         self._datastore_client = datastore.Client(project='hellodpiresworld')
 
+
+    def _is_valid_footnote(self, footnote):
+        return ('city' in footnote and 'content' in footnote)
+
     def fetch_footnotes_for_city(self, city: str) -> List[str]:
         query = self._datastore_client.query(kind='footnote')
         footnotes = query.fetch(limit=100)
-        return [f.id for f in footnotes if f['city'] in ['any', city]]
+        return [f.id for f in footnotes if (self._is_valid_footnote(f) and f['city'] in ['any', city])]
 
     def render_footnote(self, footnote_key: str) -> str:
         key = datastore.Key('footnote', footnote_key, project='hellodpiresworld')
