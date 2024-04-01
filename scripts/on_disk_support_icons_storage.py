b0ef5581b862da5d1c9b6d34cc232f1e62f059b3
Davi Pires <inhodpr@gmail.com>
2024-04-01 13:39:00 +0000

    Instantiate client only once, and with proper project set.

--- scripts/on_disk_support_icons_storage.py
+++ scripts/on_disk_support_icons_storage.py
@@ -9,9 +9,11 @@ SUPPORT_ICONS_BUCKET = 'receita-facil-support-icons'
 
 
 class OnDiskSupportIconsStorage(support_icon_storage.SupportIconsStorage):
+    def __init__(self):
+        self._client = storage.Client(project='hellodpiresworld')
+
     def fetch_support_icons_definitions(self) -> Dict[str, List[str]]:
-        client = storage.Client()
-        blobs = client.list_blobs(SUPPORT_ICONS_BUCKET)
+        blobs = self._client.list_blobs(SUPPORT_ICONS_BUCKET)
         support_icon_defs = {}
         for blob in blobs:
             category, _ = blob.name.split('/')
