ebbded1a90252ddb1fe3b1d833040977d6e799fa
Davi Rios <inhodpr@gmail.com>
2025-03-23 22:35:47 +0000

    Allowing more image types and adding a parameter for loading next version of the icons.

--- scripts/on_disk_support_icons_storage.py
+++ scripts/on_disk_support_icons_storage.py
@@ -1,24 +1,31 @@
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from scripts import storage as support_icon_storage
 
 from google.cloud import storage
 
 
-SUPPORT_ICONS_BUCKET = 'receita-facil-support-icons'
+SUPPORT_ICONS_BUCKET_V1 = 'receita-facil-support-icons'
+SUPPORT_ICONS_BUCKET_V2 = 'receita-facil-support-icons-v2'
 
 
 class OnDiskSupportIconsStorage(support_icon_storage.SupportIconsStorage):
     def __init__(self):
         self._client = storage.Client(project='hellodpiresworld')
 
-    def fetch_support_icons_definitions(self) -> Dict[str, List[str]]:
-        blobs = self._client.list_blobs(SUPPORT_ICONS_BUCKET)
+    def fetch_support_icons_definitions(
+        self,
+        version: Optional[support_icon_storage.SupportIconsStorage.Version] = None) -> Dict[str, List[str]]:
+        bucket = SUPPORT_ICONS_BUCKET_V2 if version == self.Version.V2 else SUPPORT_ICONS_BUCKET_V1
+        allowed_extensions = {'.png', '.svg'}
+        blobs = self._client.list_blobs(bucket)
         support_icon_defs = {}
         for blob in blobs:
             category, _ = blob.name.split('/')
             if category not in support_icon_defs:
                 support_icon_defs[category] = []
-            if blob.public_url and blob.public_url.endswith('.png'):
+            public_url = blob.public_url
+            extension = public_url[public_url.rfind('.'):]
+            if extension in allowed_extensions:
                 support_icon_defs[category].append(blob.public_url)
         return support_icon_defs
